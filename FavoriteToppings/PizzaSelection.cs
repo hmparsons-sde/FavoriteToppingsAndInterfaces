582fba3b85c7dcf63dc2315480f853573dd72471
Nathan Gonzalez <ncgonz@gmail.com>
2021-02-06 14:13:42 -0600

    Add project files.

---
+++ FavoriteToppings/PizzaSelection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FavoriteToppings
+{
+    class PizzaSelection
+    {
+        public List<string> Toppings { get; set; }
+    }
+}
---
+++ FavoriteToppings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+
+namespace FavoriteToppings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read a file from disk
+            var json = File.ReadAllText("./toppings.json");
+            //deserialize from json to C#
+            //property names in json and C# need to match
+            var toppingSelections = JsonConvert.DeserializeObject<List<PizzaSelection>>(json);
+
+
+            //built in but a little less feature rich.  It is faster though
+            toppingSelections = System.Text.Json.JsonSerializer.Deserialize<List<PizzaSelection>>(json);
+            
+        }
+    }
+}
